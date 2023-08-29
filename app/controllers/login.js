43cffaaed0c73ace742ab124af294f72b604404a
Francisco Agustín Micucci <famicucci@gmail.com>
2023-08-29 13:15:53 +0200

    Install cors and return a status 200 and 400 from /login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,5 +1,8 @@
 exports.login = async (req, res) => {
-	res.status(200).json({
+	res.status(200).send({
 		accessToken: 'lksdj253034mdfs*/2ferkj654*/*/2342dsx',
 	});
+	// res.status(400).send({
+	// 	detail: 'Hubo un error al intentar iniciar sesión',
+	// });
 };
--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const apiRouter = require('./routes');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
 
